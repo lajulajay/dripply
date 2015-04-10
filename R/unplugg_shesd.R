4e36ac8b240d7a73519e40f7016dd4dfd34b2d11
lajulajay <saliu@udp018649uds.ucsf.edu>
2015-04-10 11:32:45 -0700

    Added option to specify periodicity. Changed workflow/documentation to support this.

--- R/unplugg_shesd.R
+++ R/unplugg_shesd.R
@@ -1,7 +1,7 @@
 #'  Anomaly Detection in Utilities Datasets
 #'
 #'  Detecting anomalies in univariate time series data from utitlity providers.
-#'  Input is a sanitized data frame with columns TYPE, DATE, TIME, USAGE, UNITS AND TIMESTAMP
+#'   
 #'  @name unplugg_shesd
 #'  @param input_df Data frame with the following columns at a minimum
 #'    TYPE: type of data: Ex 'Electric Usage'
@@ -10,11 +10,22 @@
 #'    USAGE: usage for current activity, i.e. count
 #'    UNITS: units of usage
 #'    TIMESTAMP: timestamp for each usage entry
+#'  @param periodicity String indicating periodicity with which to slice data
+#'    options are 'second', 'minute', 'hour', 'day', 'week', 'month', 'year'
+#'  
 #'  Uses Seasonal Hybrid ESD Test at https://github.com/twitter/AnomalyDetection 
 #'  Currently optimized for PGE data available through Opower
 #'  
 
-unplugg_shesd <- function(input_df) {
+unplugg_shesd <- function(input_df, periodicity) {
+  
+  # Check periodicity
+  periodicity_options <- c('second', 'minute', 'hour', 'day', 'week', 'month', 'year')
+  if(is.character(periodicity)) {periodicity <- tolower(periodicity)}
+  else {
+    stop('Periodicity must be string')
+  }
+  if(!(periodicity %in% periodicity_options)) {stop('Invalid periodicity')}
   
   # Install package(s) as necessary
   packages <- c('lubridate', 'devtools')
@@ -34,12 +45,15 @@ unplugg_shesd <- function(input_df) {
   }
   
   # Format input content for AnomalyDetectionTs
-  anom_df <- data.frame(input_df$timestamp, input_df$usage)
+  dates <- round_date(input_df$timestamp, periodicity)
+  usage <- input_df$usage
+  input_agg <- aggregate(usage ~ dates, FUN=sum)
   
   # Invoke AnomalyDetectionTs
-  anoms = AnomalyDetectionTs(anom_df, direction='pos', alpha = 0.05, threshold='p95', 
-                             longterm=T, piecewise_median_period_weeks = 2, plot=T, y_log=T,
+  anoms = AnomalyDetectionTs(input_agg, direction='pos', alpha = 0.05, plot=T, 
                              ylabel=paste(input_df$type[1], ' ', '(', input_df$units[1], ')')) 
   anoms$plot
-  return(anoms)
+  
+  
+
 }
